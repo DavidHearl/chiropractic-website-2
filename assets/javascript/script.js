5502c82be8b024004ee6c9ca0b7d3e43f661ecf4
DavidHearl <david.hearl@outlook.com>
2022-11-28 17:01:36 +0000

    adjusted order and styling of content section

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,7 +18,6 @@ document.addEventListener('DOMContentLoaded', function() {
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
